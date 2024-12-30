d12483698132495cef9af96d91bafdfddf0429df
Dhruv <Dhruv@Dhruv-Desktop>
2024-12-30 10:07:41 +0530

    add b

--- demo/src/com/A.java
+++ demo/src/com/A.java
@@ -1,9 +1,11 @@
 package com;
 
 public class A {
-int a;
-public A() {
-	// TODO Auto-generated constructor stub
-}
+	int a;
+	int b;
+
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
